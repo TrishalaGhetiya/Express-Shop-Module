8225113304dcddc2d316a8d892e96282d17241ed
Trishala Ghetiya <trishalaghetiya@gmail.com>
2023-08-12 16:00:42 +0530

    added relationships

--- app.js
+++ app.js
@@ -1,12 +1,13 @@
 const path = require('path');
-const cors = require('cors');
+//const cors = require('cors');
 
 const express = require('express');
 const bodyParser = require('body-parser');
 
 const errorController = require('./controllers/error');
-
 const sequelize = require('./util/database');
+const Product = require('./models/product');
+const User = require('./models/user');
 
 const app = express();
 
@@ -17,24 +18,45 @@ const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const userRoutes = require('./routes/user');
 const expenseRoutes = require('./routes/expense');
-app.use(cors());
+//app.use(cors());
 
-app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(expenseRoutes);
+app.use((req, res, next) => {
+    User.findByPk(1)
+        .then(user => {
+            req.user = user;
+            next();
+        })
+        .catch(err => console.log(err))
+});
+
+
 app.use(userRoutes);
 app.use('/admin', adminRoutes);
-//app.use(shopRoutes);
+app.use(shopRoutes);
+app.use(expenseRoutes);
 
 app.use(errorController.get404);
 
-
+Product.belongsTo(User, {constraints: true, onDelete: 'CASCADE'});
+User.hasMany(Product);
 
 sequelize
-    .sync()
+    .sync({force: true})
     .then(result => {
+        return User.findByPk(1);
         //console.log(result);
+    })
+    .then(user => {
+        if(!user){
+           return User.create({userName: 'Trishala', email: 'trishala@gmail.com'});
+        }
+        return user;
+    })
+    .then(user => {
+        //console.log(user);
         app.listen(3000);
     })
     .catch(err => console.log(err));
